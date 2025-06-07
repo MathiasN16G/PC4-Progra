eacd98ddbb7e7dacd12d1208055d048f0bb948fc
Mathias <mathias_nunez@usmp.pe>
2025-06-07 16:39:28 -0500

    Diseño de la pagina

--- obj/Debug/net9.0/EvaluadorInteligente.AssemblyInfo.cs
+++ obj/Debug/net9.0/EvaluadorInteligente.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvaluadorInteligente")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa75d00e8d15960702e2110b74400546de99b4e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d095adaa59c4e6542b966bdb76b1e1772d5da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvaluadorInteligente")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvaluadorInteligente")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
